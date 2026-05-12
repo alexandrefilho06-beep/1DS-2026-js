6e2b850590db2fa3cbbbe9f366719170f5e9d4bd
MESTRE_LINGUIÇA <alexandre.filho06@escola.pr.gov.br>
2026-05-12 08:30:37 -0300

    Add files via upload

---
+++ aula01.js
@@ -0,0 +1,7 @@
+//alert("0 carpa capim fica batendo no amiguinho")
+
+console.log("primeiro código em JS");
+console.log(4+5);
+console.log( 3 * 4);
+console.log(true);
+console.log(3.89)
---
+++ aula02.js
@@ -0,0 +1,10 @@
+//DEEFINIÇÃO DE VARIÁVEIS
+
+let pessoa = "nome da pessoa";
+let idadePessoa = 30;
+let alturaPessoa = 1.93;
+let statusPessoa = true;
+let salarioPessoa = 1500;
+let salarioBonus = 100;
+
+let salarioTotal = salarioPessoa + salarioBonus;
