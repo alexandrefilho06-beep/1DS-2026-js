da1c6e6662c017287e5a42a2d02fa8fa6bc1be63
MESTRE_LINGUIÇA <alexandre.filho06@escola.pr.gov.br>
2026-08-28 08:11:52 -0300

    Implement voting eligibility check
    Added conditional checks for voting eligibility based on age.

---
+++ aula03.js
@@ -0,0 +1,36 @@
+let nome = 'alexandre';
+
+if (nome == 'alexandre') {
+    console.log('Voce digitou o nome correto!');
+} else {
+    console.log('Voce digitou outra coisa...!')
+}
+
+let idade = 18;
+
+if (idade >= 18) {
+    console.log('Voce é praticamnete um adulto...');
+} else if (idade < 18) {
+    console.log('Voce não é maior de idade...');
+} else {
+    console.log('Voce é um adulto...');
+}
+
+//FAÇA UMA VERIFICAÇAO COM IF E ELSE
+// DE UMA PESSOA SE ELA PODE OU NÃO PODE VOTAR.
+
+let nome = 'alexandre';
+
+if (nome == 'alexandre') {
+    console.log('Voce digitou o nome correto!');
+} else {
+    console.log('Voce digitou outra coisa...!')
+}
+
+let idade = 18;
+
+if (idade >= 18) {
+    console.log('Voce pode votar...');
+} else if (idade < 18) {
+    console.log('Voce não pode votar...');
+} 
